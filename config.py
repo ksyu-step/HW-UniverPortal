3e8c442cb165c8587b9ea24cec9ffa4fc47657b4
ksyu <ksyu@ytechteam.com>
2021-09-09 12:25:31 +0300

    some chenges in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e8ab28febef57d301efec25175788a77'  # ksyu
+app.config['SECRET_KEY'] = 'e8ab28febef57d301efec25175788a77'  #
 app.config['TEMPLATE_AUTO_RELOAD'] = True
